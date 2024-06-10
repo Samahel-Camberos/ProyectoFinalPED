45b2c10c4f0b9adf7e0e19efb670b3c5762233b6
Samahel Camberos <162946770+Samahel-Camberos@users.noreply.github.com>
2024-06-09 20:59:20 -0700

    Update scrapping_amazon.py

--- scrapping_amazon.py
+++ scrapping_amazon.py
@@ -1,3 +1,10 @@
+"""
+Este script se encarga de recopilar información sobre tenis Nike disponibles en la página
+ web de Amazon. Utiliza técnicas de web scraping para extraer datos relevantes como el nombre
+  del producto, precio, calificaciones, número de opiniones y URL del producto.
+  Los datos extraídos se guardan en un archivo CSV para su posterior análisis.
+
+"""
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,15 +17,18 @@ from bs4 import BeautifulSoup
 import time
 
 def boot_en_amazon1(cantidad, busqueda):
-    # Configurar el servicio de GeckoDriver
-    service = Service(GeckoDriverManager().install())
+    # Configurar el servicio de GeckoDriver/Chrome
+    service = Service(ChromeDriverManager().install())
+    #service = Service(GeckoDriverManager().install())
 
     # Configurar opciones del navegador
-    options = webdriver.FirefoxOptions()
+    options = webdriver.ChromeOptions()
+    #options = webdriver.FirefoxOptions()
     options.add_argument("--window-size=1000,1200")
 
     # Crear instancia del navegador
-    driver = webdriver.Firefox(service=service, options=options)
+    driver = webdriver.Chrome(service=service, options=options)
+    #driver = webdriver.Firefox(service=service, options=options)
 
     # Abrir Amazon
     driver.get("https://www.amazon.com.mx/ref=nav_logo")
