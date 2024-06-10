34acda64fb636b80e27831a168b863c5c837e5a3
Samahel Camberos <162946770+Samahel-Camberos@users.noreply.github.com>
2024-06-09 20:56:30 -0700

    Update scrapping_amazon.py

--- scrapping_amazon.py
+++ scrapping_amazon.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.firefox.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+#from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
 import time
 
